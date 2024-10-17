e76271b55ed0b08fb54cbb17e9cdea7c9c6998e3
Alvaromn1104 <alvaromn1104@gmail.com>
2024-10-17 11:29:57 +0200

    guardado java con excecion

--- main.java
+++ main.java
@@ -1,3 +1,13 @@
-public main{
-		public static void main(String[])
+public class main{
+		public static void main(String[] args){
+		try{
+			int[] miArray = new int[3];
+			for (int numero : miArray){
+			System.out.println(numero);
+
+}
+}catch (Exception e){
+System.out.println("Error" +e);
+}
+}
 }
