cc0d004dd2f0ba0cc374d04991778eab9a24e59c
Alvaromn1104 <alvaromn1104@gmail.com>
2024-10-17 10:44:08 +0200

    impresion de argumentos en main

--- main.java
+++ main.java
@@ -1,3 +1,12 @@
 public main{
 		public static void main(String[])
+			System.out.print(args.lenght);
+			if(args.lentght == 0){
+				System.out.print("No hay argumentos");
+
+}else {
+			System.out.println("hay" + args.length() + "argumentos");
+}
+
+
 }
